ca22056f01293d11164e18889b192dd8743ef2f6
youneskarir <youneskarir@gmail.com>
2024-05-07 19:14:11 +0100

    initial commit

---
+++ src/main/java/com/younes/karir/HttpTests/HttpTestsApplication.java
@@ -0,0 +1,13 @@
+package com.younes.karir.HttpTests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpTestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/younes/karir/HttpTests/HttpTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.younes.karir.HttpTests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
